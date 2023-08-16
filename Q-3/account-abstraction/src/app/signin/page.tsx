e9ff510e8f5a8f837e5bac21ee280ff58dc5d065
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-08-17 01:26:48 +0500

    login finalized

--- Q-3/account-abstraction/src/app/signin/page.tsx
+++ Q-3/account-abstraction/src/app/signin/page.tsx
@@ -6,24 +6,31 @@ import Link from "next/link"
 
 const Signin = () => {
   return (
-    <div>
-        <div>
-            <h1>Welcome to JWT Authenctication</h1>
-            <p>Signup</p>
+    <div className="h-screen bg-black/[0.10]">
+    <div className="font-heading flex justify-center items-center">
+        <div className="flex bg-gray-400/[0.30] mt-[10rem] w-[35rem] py-4 rounded-[5rem] shadow-xl z-10 flex-col justify-center items-center ">
+        <div className="">
+            <h1 >Welcome to JWT Authenctication</h1>
+            <p className="text-orange-400 text-3xl font-mainlog font-semibold items-start text-start">Sigin</p>
         </div>
-        <div>
+        <div className="flex flex-col py-4 space-y-2 w-[30rem]">
             <Label>Email:</Label>
-            <Input type='email' placeholder='Enter here your email'/>
+            <Input className="" type='email' placeholder='Enter here your email'/>
             <Label>Password:</Label>
-            <div>
+            <Input type="password" placeholder="Provide your password" maxLength={8}/>
+            <div className="py-2 flex justify-center">
                 <Link href={""} >
                 <Button>Signup</Button>
                 </Link>
             </div>   
         </div>
         <div>
-            <p>If don't account  an account <Link className='text-blue-900 underline' href={"/signup"}> click here </Link></p>
+        <p>If forgot the password <Link className='text-blue-900 underline' href={""}> click here    </Link></p>
+        <p>If don't have an account  an account <Link className='text-blue-900 underline' href={"/signup"}> click here </Link></p>
+
         </div>
+        </div>
+    </div>
     </div>
   )
 }
