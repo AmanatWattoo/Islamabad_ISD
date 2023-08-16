04883ad634fb05a789e0990136ad4f063af97420
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-08-17 00:48:32 +0500

    added

--- Q-3/account-abstraction/src/app/signin/page.tsx
+++ Q-3/account-abstraction/src/app/signin/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div>
+        <div>
+            <h1>Welcome to JWT Authenctication</h1>
+            <p>Signup</p>
+        </div>
+        <div>
+            <Label>Email:</Label>
+            <Input type='email' placeholder='Enter here your email'/>
+            <Label>Password:</Label>
+            <div>
+                <Link href={""} >
+                <Button>Signup</Button>
+                </Link>
+            </div>   
+        </div>
+        <div>
+            <p>If don't account  an account <Link className='text-blue-900 underline' href={"/signup"}> click here </Link></p>
+        </div>
+    </div>
   )
 }
 
--- Q-3/account-abstraction/src/app/signup/page.tsx
+++ Q-3/account-abstraction/src/app/signup/page.tsx
@@ -1,36 +1,50 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React from "react";
 
 const Signup = () => {
   return (
     <div>
+      <div>
+        <h1>Welcome to JWT Authenctication</h1>
+        <p>Signup</p>
+      </div>
+      <div>
+        <Label>Username:</Label>
+        <Input type="text" placeholder="Enter here your name" />
+        <Label>Email:</Label>
+        <Input type="email" placeholder="Enter here your email" />
+        <Label>Password:</Label>
+        <Input
+          type="Password"
+          placeholder="Enter here your password"
+          maxLength={8}
+        />
+        <Label> Re_Type Password:</Label>
+        <Input
+          type="Password"
+          placeholder="Again write pasword"
+          maxLength={8}
+        />
         <div>
-            <h1>Welcome to JWT Authenctication</h1>
-            <p>Signup</p>
+          <Link href={""}>
+            <Button>Signup</Button>
+          </Link>
         </div>
-        <div>
-            <Label>Username:</Label>
-            <Input type='text' placeholder='Enter here your name'/>
-            <Label>Email:</Label>
-            <Input type='email' placeholder='Enter here your email'/>
-            <Label>Password:</Label>
-            <Input type='Password' placeholder='Enter here your password' maxLength={8}/>
-            <Label> Re_Type Password:</Label>
-            <Input type='Password' placeholder='Again write pasword' maxLength={8}/>
-            <div>
-                <Link href={""} >
-                <Button>Signup</Button>
-                </Link>
-            </div>   
-        </div>
-        <div>
-            <p>If already have  an account <Link className='text-blue-900 underline' href={"/signin"}> click here </Link></p>
-        </div>
+      </div>
+      <div>
+        <p>
+          If already have an account{" "}
+          <Link className="text-blue-900 underline" href={"/signin"}>
+            {" "}
+            click here{" "}
+          </Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
