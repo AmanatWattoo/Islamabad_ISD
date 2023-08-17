50a0fc80e5bb259a5562f3ba7a505350b7afa46e
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-08-17 23:45:47 +0500

    علم کا خزانہ

--- Q-3/vercel-connection/src/app/signin/page.tsx
+++ Q-3/vercel-connection/src/app/signin/page.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 const Signin = () => {
   return (
-    <div className="h-screen bg-black/[0.10]">
+    <div className="h-screen bg-black/[0.10] w-full">
     <div className="font-heading flex justify-center items-center">
-        <div className="flex bg-gray-400/[0.30] mt-[10rem] w-[35rem] py-4 rounded-[5rem] shadow-xl z-10 flex-col justify-center items-center ">
+        <div className="flex bg-gray-400/[0.30] md:mt-[10rem] mt-[10rem] sm:mt-[20rem] xl:mt-[20rem] w-[35rem]  py-4 rounded-[5rem] shadow-xl z-10 flex-col justify-center items-center ">
         <div className="max-w-[35rem]">
             <h1 className='text-3xl'>Welcome to JWT Authentication</h1>
             <p className="text-orange-400 text-3xl font-mainlog font-semibold items-start text-start">Signin</p>
