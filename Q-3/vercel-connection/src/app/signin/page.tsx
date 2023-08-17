1f141fbf71cbdf5dbe75c63ec24640ffa98f7f6e
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-08-17 22:42:34 +0500

    added

--- Q-3/vercel-connection/src/app/signin/page.tsx
+++ Q-3/vercel-connection/src/app/signin/page.tsx
@@ -6,17 +6,17 @@ const Signin = () => {
     <div className="font-heading flex justify-center items-center">
         <div className="flex bg-gray-400/[0.30] mt-[10rem] w-[35rem] py-4 rounded-[5rem] shadow-xl z-10 flex-col justify-center items-center ">
         <div className="max-w-[35rem]">
-            <h1 className='text-2xl'>Welcome to JWT Authentication</h1>
+            <h1 className='text-3xl'>Welcome to JWT Authentication</h1>
             <p className="text-orange-400 text-3xl font-mainlog font-semibold items-start text-start">Signin</p>
         </div>
         <div className="flex flex-col py-4 space-y-2 w-[30rem]">
             <p>Email:</p>
-            <input className="" type='email' placeholder='Enter here your email'/>
+            <input className="rounded-md h-[2rem] px-2 outline-none" type='email' placeholder='Enter here your email'/>
             <p>Password:</p>
-            <input type="password" placeholder="Provide your password" maxLength={8}/>
+            <input className='rounded-md h-[2rem] px-2 outline-none' type="password" placeholder="Provide your password" maxLength={8}/>
             <div className="py-2 flex justify-center">
                 <Link href={""} >
-                <button>Signin</button>
+                <button className='bg-blue-900 text-white p-2 rounded-md w-[8rem]'>Signin</button>
                 </Link>
             </div>   
         </div>
--- Q-3/vercel-connection/src/app/signup/page.tsx
+++ Q-3/vercel-connection/src/app/signup/page.tsx
@@ -6,22 +6,22 @@ const Signup = () => {
     <div className="h-screen bg-black/[0.10]">
     <div className="font-heading flex justify-center items-center">
         <div className="flex bg-gray-400/[0.30] mt-[6rem] w-[35rem] py-4 rounded-[5rem] shadow-xl z-10 flex-col justify-center items-center ">
-        <div className="">
-            <h1 >Welcome to JWT Authentication</h1>
+        <div className="max-w-[35rem]">
+            <h1 className='text-3xl'>Welcome to JWT Authentication</h1>
             <p className="text-orange-400 text-3xl font-mainlog font-semibold items-start text-start">Signup</p>
         </div>
         <div className="flex flex-col py-4 space-y-2 w-[30rem]">
             <p>Username:</p>
-            <input className="" type='text' placeholder='Enter here your name'/>
+            <input className="rounded-md h-[2rem] px-2 outline-none" type='text' placeholder='Enter here your name'/>
             <p>Email:</p>
-            <input className="" type='email' placeholder='Enter here your email'/>
+            <input className="rounded-md h-[2rem] px-2 outline-none" type='email' placeholder='Enter here your email'/>
             <p>Password:</p>
-            <input type="password" placeholder="Provide your password" maxLength={8}/>
+            <input className='rounded-md h-[2rem] px-2 outline-none' type="password" placeholder="Provide your password" maxLength={8}/>
             <p> Confirm Password:</p>
-            <input type="password" placeholder="Enter again password" maxLength={8}/>
+            <input className='rounded-md h-[2rem] px-2 outline-none' type="password" placeholder="Enter again password" maxLength={8}/>
             <div className="py-2 flex justify-center">
                 <Link href={""} >
-                <button>Signup</button>
+                <button className='bg-blue-900 text-white p-2 rounded-md w-[8rem]'>Signup</button>
                 </Link>
             </div>   
         </div>
