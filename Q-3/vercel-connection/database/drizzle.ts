256eeb67558ca1dc3ce033040cd91baf2d82d1a3
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-08-18 10:43:10 +0500

    database connection

---
+++ Q-3/vercel-connection/database/drizzle.ts
@@ -0,0 +1,14 @@
+import { sql } from "@vercel/postgres";
+import { varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+
+export const UserSignUp = pgTable(
+    "signup",{
+        Name:varchar("Name").notNull(),
+        Email: varchar("Email").notNull(),
+        Password: varchar("Password").notNull(),
+    }
+)
+export const db = drizzle(sql)
+//use this object to send quries to your database
