445c5f7d2c2f389e3e1f4687811cae0c0ffc7518
AmanatWattoo <80233118+AmanatWattoo@users.noreply.github.com>
2023-09-21 23:10:49 +0500

    submiutted

---
+++ Q-3/Langchain/class_1/index.js
@@ -0,0 +1,13 @@
+import { OpenAI } from 'langchain/llms/openai';
+import 'dotenv/config';
+//  console.log(process.env.OPENAI_API_KEY);
+const llm = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0.9,
+});
+async function main() {
+    const result = await llm.predict(`Thanks good night`);
+    // Handle the result...
+    console.log(result);
+}
+main();
